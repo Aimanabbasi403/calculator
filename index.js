b4d55526c80179e57df6401fbd8a9e143b0be976
aiman abbasi <aimanabbasi403@gmail.com>
2024-04-28 03:52:28 -0700

    simple calculator

---
+++ index.js
@@ -0,0 +1,32 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    { message: "enter First Number", type: "number", name: "firstNumber" },
+    { message: "enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one operator to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division",],
+    },
+]);
+const { firstNumber, secondNumber, operator } = answers;
+let result;
+switch (operator) {
+    case "addition":
+        result = firstNumber + secondNumber;
+        break;
+    case "subtraction":
+        result = firstNumber - secondNumber;
+        break;
+    case "multiplication":
+        result = firstNumber * secondNumber;
+        break;
+    case "division":
+        result = firstNumber / secondNumber;
+        break;
+    default:
+        console.log("Invalid operation");
+}
+;
+console.log(`result: ${firstNumber} ${operator} ${secondNumber}  = ${result}`);
